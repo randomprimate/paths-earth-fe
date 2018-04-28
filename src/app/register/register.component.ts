9f2d1a75ecbb8f48e96d7a5b3b836c47a3310e82
Jose Torres <jose.torres@runtastic.com>
2018-04-29 01:56:16 +0200

    Adds registration form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers, RequestOptions } from "@angular/http";
+import { Router } from "@angular/router";
+import "rxjs/Rx";
 
 @Component({
   selector: 'app-register',
@@ -7,7 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public input: any;
+ 
+  public constructor(private http: Http, private router: Router) {
+      this.input = {
+          "firstname": "",
+          "lastname": "",
+          "email": "",
+          "password": ""
+      };
+  }
+
+  public register() {
+      if(this.input.email && this.input.password) {
+          let headers = new Headers({ "content-type": "application/json" });
+          let options = new RequestOptions({ headers: headers });
+          this.http.post("http://localhost:3000/account", JSON.stringify(this.input), options)
+              .map(result => result.json())
+              .subscribe(result => {
+                  this.router.navigate(["/login"]);
+              });
+      }
+  }
 
   ngOnInit() {
   }
